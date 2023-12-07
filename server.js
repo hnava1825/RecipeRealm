e2cc77e9f37fcef5e3c8ec8bf6cbb10b474e3c12
Hector Nava <111790253+hnava1825@users.noreply.github.com>
2023-12-07 12:09:41 -0700

    Update server.js
    fixing config issues

--- server.js
+++ server.js
@@ -1,26 +1,42 @@
 const express = require('express');
 const fetch = require('node-fetch');
-const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-const CACHE_TTL = 10 * 60 * 1000; // 10 minutes
-const cache = {};
+// Middleware to parse JSON requests
+app.use(express.json());
 
-require('dotenv').config();
-// Replace with your actual API endpoint
-const appid = process.env.APP_ID;
-const appKey = process.env.APP_KEY;
-const baseUrl = `https://api.edamam.com/api/recipes/v2?type=public&app_id=${appid}&app_key=${appKey}`;
+// Your API key and other configurations
+const YOUR_API_KEY = 'YOUR_API_KEY'; // Replace with your actual API key
+const baseUrl = `https://api.edamam.com/search?app_id=YOUR_APP_ID&app_key=${YOUR_API_KEY}`;
+const CACHE_TTL = 3600000; // Cache TTL in milliseconds (1 hour)
+const cache = {}; // Cache object to store responses
 
-// Use CORS middleware
-app.use(cors());
+// Route to fetch recipes
+app.get('/recipes', async (req, res) => {
+    const type = req.query.type || 'Meal Prep';
+    const url = `${baseUrl}&q=${type}`;
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!'); // Replace with your desired response
+    // Use cached data if available
+    if (cache[url] && (Date.now() - cache[url].timestamp) < CACHE_TTL) {
+        return res.json(cache[url].data);
+    }
+
+    try {
+        const apiRes = await fetch(url);
+        const data = await apiRes.json();
+        cache[url] = { data, timestamp: Date.now() };
+        res.json(data);
+    } catch (error) {
+        console.error("Error fetching recipes:", error);
+        res.status(500).json({ error: 'Failed to fetch recipes.' });
+    }
 });
 
+// Serve static files (e.g., HTML, CSS, JavaScript)
+app.use(express.static('public'));
 
-app.listen(3001, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
